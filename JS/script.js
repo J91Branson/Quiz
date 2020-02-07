8167dd1259e72fcb413fafdbe0ef89247e99d0e8
Joshua <j91branson@gmail.com>
2020-02-06 21:40:57 -0600

    user input functionality built

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+function submitAnswers() {
+    var total = 10;
+    var score = 0;
+
+
+    //Get user input
+
+    var q1 = document.forms["quizForm"]["q1"].value;
+    var q2 = document.forms["quizForm"]["q2"].value;
+    var q3 = document.forms["quizForm"]["q3"].value;
+    var q4 = document.forms["quizForm"]["q4"].value;
+    var q5 = document.forms["quizForm"]["q5"].value;
+    var q6 = document.forms["quizForm"]["q6"].value;
+    var q7 = document.forms["quizForm"]["q7"].value;
+    var q8 = document.forms["quizForm"]["q8"].value;
+    var q9 = document.forms["quizForm"]["q9"].value;
+    var q10 = document.forms["quizForm"]["q10"].value;
+
+
+    alert(q1);
+    }
